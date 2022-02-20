8a9f2bcf1cca68b6e9b4949c5d584757ddb3d45a
RITIK MAKHIJA <ritikmakhija2002@gmail.com>
2022-02-20 16:08:44 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+import Script from "next/script";
 
 const supportedChainIds = [4];
 const connectors = {
@@ -12,6 +13,12 @@ function MyApp({ Component, pageProps }) {
       supportedChainIds={supportedChainIds}
       connectors={connectors}
     >
+      <Script>
+        alert( 'This website includes\n 1.Connect Wallet\n 2.send/receive own
+        created coins\n 3.calculate balance of available balance\n Note :-
+        Thirdweb is used to create this application and send, recive, qr code
+        and copy wallet address option is created only' )
+      </Script>
       <Component {...pageProps} />
     </ThirdwebWeb3Provider>
   );
